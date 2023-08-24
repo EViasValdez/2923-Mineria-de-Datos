8e47d73dae46dcb9879fad9bc1205a958191f1db
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-08-24 10:18:47 -0600

    Add files via upload

---
+++ ServidorWeb/ServidorWeb/Controllers/AritmeticaController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ServidorWeb.Controllers
+{
+    public class AritmeticaController : ApiController
+    {
+        [ActionName("Sumar")]
+        [HttpGet]
+
+        public int Sumar(int id)
+        {
+            int Resultado = id + 10;
+            return Resultado;
+        }
+
+        [ActionName("Restar")]
+        [HttpGet]
+        public int Restar(int id)
+        {
+            int Resultado = id + 10;
+            return Resultado;
+        }
+
+        [ActionName("Multiplicar")]
+        [HttpGet]
+        public int Multiplicar(int id)
+        {
+            int Resultado = id + 10;
+            return Resultado;
+        }
+
+        [ActionName("Dividir")]
+        [HttpGet]
+        public int Dividir(int id)
+        {
+            int Resultado = id + 10;
+            return Resultado;
+        }
+    }
+}
---
+++ SpiderDemo/SpiderDemo/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderDemo.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Price { get; set; }
+        public string Link { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
