f73cc7908dbbfa4a0de8d87a3d781f392da1c6b5
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-28 09:35:14 -0600

    Add files via upload

--- PruebaML/PruebaML/obj/Debug/net6.0/PruebaML.AssemblyInfo.cs
+++ PruebaML/PruebaML/obj/Debug/net6.0/PruebaML.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc5539f1ce19c3411bcd2ba25d7f575f9089fb3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9280e6830ebff64f072a5ebe73928e9f483fa2a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
