07819fe4035e4005da56caaadcf747b56dca5977
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-04-12 21:29:06 -0600

    Add files via upload

--- AplicacionWeb/AplicacionWeb/Program.cs
+++ AplicacionWeb/AplicacionWeb/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionWeb
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
