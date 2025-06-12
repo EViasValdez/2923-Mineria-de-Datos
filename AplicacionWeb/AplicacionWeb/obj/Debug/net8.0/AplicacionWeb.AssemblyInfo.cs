905a3e8f346d75eb20b4d9ab77edda528dd5ca87
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-06-11 19:10:00 -0600

    Add files via upload

--- AplicacionWeb/AplicacionWeb/obj/Debug/net8.0/AplicacionWeb.AssemblyInfo.cs
+++ AplicacionWeb/AplicacionWeb/obj/Debug/net8.0/AplicacionWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e54aa5a557144091b44daba3fd10a10fbfeb288d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7912d7d65f422db7c9cc36751fa6d05aab04704b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
